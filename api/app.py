5dea92ff33fed3d987ab2c8a5e561c95c2093fe3
andreluizsilv <andre_luiz29@hotmail.com>
2024-10-20 15:42:19 -0300

    idade/dinamica

--- api/app.py
+++ api/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'andreluizsilv'
 
+# Data de nascimento
+data_nascimento = datetime(1976, 3, 11)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    hoje = datetime.now()
+    idade = hoje.year - data_nascimento.year - ((hoje.month, hoje.day) < (data_nascimento.month, data_nascimento.day))
+
+    return render_template('index.html', idade=idade)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
